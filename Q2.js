e4b5a1a9a83fd48a89c41f4f2fbbb021f298b8ad
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-17 19:16:42 +0500

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,5 @@
+"use strict";
+// Q2:Personal message: Store a prson's name in a variable, and print a message to that person. Your message should be simple, such as, 'Hello Eric, 
+// would you like to learn some python today?"
+let name1 = "Eric";
+console.log(`Hello ${name1}, "would you like to learn some python today?"`);
